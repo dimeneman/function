daeb0554cf29ba092a93fc6303713319faee9e30
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-05-11 20:13:09 +0530

    Description
    Documentation in Python refers to the written text that accompanies a Python software project. This text explains the purpose and use of the code, making it easier for others (and often yourself in the future) to understand and maintain.

---
+++ documentation.py
@@ -0,0 +1,5 @@
+def greeetings():
+    ''' This is  a function to greet'''  # this is a documentation for my function ( we've to use it right after own function)
+    print('Hello')
+    print('Hi')
+help(greeetings)  # this will print the documentation..
