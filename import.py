307718c3f961d5a03d1083387d0e6675069cba2d
dimeneman <146258763+dimeneman@users.noreply.github.com>
2024-05-11 20:20:41 +0530

    Description
    We should know how to import a file or library in python  . Because while working  on data analysis we need to import libraries like numpy , pandas , matplotlib , csv file ( real time dataset ) etc.
    So it would be beneficial  for us to know how to import a library. 
    In this file simple math library is imported.

---
+++ import.py
@@ -0,0 +1,8 @@
+''' there are many in build module for python and that's are the part of the standard 
+library... this modules helps are to use many functions.. to use then we need to 
+import the modules/libraries'''
+
+import math  # helps us to use many mathematical functions
+help(math.ceil) # ceil ==> to get the nearest integer
+
+print(math.ceil(1.2)) 
